6c7de170c76ab4d9e161fde8fb432d04b2698c5b
stett <stettjawa@gmail.com>
2015-03-11 13:25:17 -0700

    ugh

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -5,7 +5,9 @@ from accounts.settings import MELLOW_ADMIN_ROLE
 
 class ImpersonateMiddleware(object):
     def process_request(self, request):
-        if request.user.is_authenticated() and Role.get_role(request.user.role).supersedes(MELLOW_ADMIN_ROLE):
+        user_role = Role.get_role(request.user.role)
+        admin_role = Role.get_role(MELLOW_ADMIN_ROLE)
+        if request.user.is_authenticated() and user_role.supersedes(admin_role):
             if '__impersonate' in request.GET:
                 request.session['impersonate_id'] = int(request.GET['__impersonate'])
             elif '__unimpersonate' in request.GET:
