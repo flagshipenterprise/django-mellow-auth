f83c0c7e6509d16c8344039d075b1f8ba0c7b761
stett <stettjawa@gmail.com>
2015-05-09 09:36:40 -0700

    added testproject to the repo

--- mellow/models.py
+++ mellow/models.py
@@ -54,7 +54,8 @@ class Account(AbstractBaseUser):
         blank=True, help_text='The groups this user belongs to. A user will '
                               'get all permissions granted to each of '
                               'their groups.',
-        related_name="user_set", related_query_name="user")
+        related_name="user_set",
+        related_query_name="user")
 
     date_joined = models.DateTimeField(default=timezone.now)
 
